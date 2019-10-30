5a64ab8bb6710777ca158f9304526bbe5abec0bb
wouwou4444 <valery.buz@outlook.com>
2019-10-30 14:20:00 +0100

    Frontier, CML, MSR minimization scipy.optimize

--- erk.py
+++ erk.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import scipy.stats
+from scipy.optimize import minimize
 
 def drawdown(return_series: pd.Series):
     """
@@ -36,7 +39,7 @@ def get_ind_returns():
     rets = me_m / 100
     rets.index = pd.to_datetime(rets.index, format = "%Y%m").to_period("M")
     rets.index.name = "Period"
-    rets.columns = list(rets.columns.str.strip())
+    rets.columns = (rets.columns.str.strip())
     
     return rets
 
@@ -69,6 +72,8 @@ def semideviation(r):
 
 
 import numpy as np
+
+##################################################################################
 def var_historic(r, level = 5):
     """
     VaR Historic
@@ -79,7 +84,13 @@ def var_historic(r, level = 5):
         return -np.percentile(r, level, axis = 0)
     else:
         raise TypeError("Expected r to be a pandas Series or DataFrame")
-        
+
+
+def cvar_historic(r, level = 5):
+    is_beyond = r <= -var_historic(r, level = level)
+    return -r[is_beyond].mean()
+
+##################################################################################
 from scipy.stats import norm
 def var_gaussian(r, level = 5, modified = False):
     z_norm = norm.ppf(level/100)
@@ -94,3 +105,128 @@ def var_gaussian(r, level = 5, modified = False):
                  )
     
     return -(r.mean() + z_norm_mod * r.std(ddof = 0))
+
+def cvar_gaussian(r, level = 5, modified = False):
+    is_beyond = r <= -var_gaussian(r, level = level, modified = modified)
+    return -r[is_beyond].mean()
+
+##################################################################################
+def annualize_ret(r, periods_per_year = 12):
+    compound_growth = (1 +r).prod()
+    n_periods = r.shape[0]
+    return compound_growth**(periods_per_year/n_periods) - 1
+
+def annualize_vol(r, periods_per_year = 12):
+    return r.std(ddof=0) * (periods_per_year**0.5)
+
+
+def sharpe_ratio(r, riskfree_rate,periods_per_year):
+    rf_per_period = (1 + riskfree_rate)**(1/periods_per_year) - 1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = annualize_ret(excess_ret, periods_per_year)
+    ann_vol = annualize_vol(r, periods_per_year)
+    return ann_ex_ret/ann_vol
+
+
+
+##################################################################################
+
+##  Efficient Frontier
+
+##################################################################################
+def portfolio_return(weights, returns):
+    return weights.T @ returns
+
+
+def portfolio_vol(weights, cov):
+    return (weights.T @ cov @ weights )**0.5
+
+def plot_ef2(n_points, er, cov, style = ".-"):
+    if er.shape[0] != 2 or cov.shape[0] != 2:
+        raise ValueError('can only plot 2-asset Frontiers')
+    weights = [np.array([w, 1 -w]) for w in np.linspace(0,1, n_points)]
+    rets = [ portfolio_return(w, er) for w in weights]
+    vols = [ portfolio_vol(w, cov) for w in weights]
+    ef = pd.DataFrame({"R": rets, "Vol": vols})
+    return ef.plot.line(x = "Vol", y = "R", style = style, figsize = (16, 6))
+
+
+def plot_ef(n_points, er, cov, style = ".-"):
+
+    weights = optimal_weights(n_points, er, cov) # ???
+    
+    rets = [ portfolio_return(w, er) for w in weights]
+    vols = [ portfolio_vol(w, cov) for w in weights]
+    ef = pd.DataFrame({"R": rets, "Vol": vols})
+    return ef.plot.line(x = "Vol", y = "R", style = style, figsize = (16, 6))
+
+
+def optimal_weights(n_points, er, cov):
+    target_rs = np.linspace(er.min(), er.max(), n_points)
+    weights = [ minimize_vol(target_return, er, cov) for target_return in target_rs]
+    return weights
+
+
+def minimize_vol(target_return, er, cov, disp = False):
+    """
+    target_return -> weight vector
+    """
+    n = er.shape[0]
+    init_guess = np.repeat(1/n, n)
+    bounds = ((0.0, 1.0),) * n
+    return_is_target = {
+        "type": "eq",
+        "args": (er,),
+        "fun": lambda w, er: target_return - portfolio_return(w, er)
+        }
+    weights_sum_to_1 = {
+        "type": "eq",
+        "fun": lambda w: np.sum(w) - 1
+    }
+    results = minimize(portfolio_vol, init_guess,
+                      args = (cov,),
+                      method="SLSQP",
+                      options={'disp':  False},
+                      constraints = (return_is_target, weights_sum_to_1),
+                       bounds = bounds
+                      )
+    #weights in results.x
+    if disp:
+        return n, er, init_guess, bounds, return_is_target, results, results.x
+    else:
+        return results.x
+    
+
+
+def msr(riskfree_rate, er, cov, disp = False):
+    """
+    RiskFree Rate, er, cov -> msr
+    """
+    n = er.shape[0]
+    init_guess = np.repeat(1/n, n)
+    bounds = ((0.0, 1.0),) * n
+
+    weights_sum_to_1 = {
+        "type": "eq",
+        "fun": lambda w: np.sum(w) - 1
+    }
+    def neg_sharpe_ratio(w, riskfree_rate, er, cov):
+        r = portfolio_return(w, er)
+        vol = portfolio_vol(w, cov)
+        return -(r - riskfree_rate)/vol
+        
+    results = minimize(neg_sharpe_ratio, init_guess,
+                      args = (riskfree_rate, er, cov,),
+                      method="SLSQP",
+                      options={'disp':  False},
+                      constraints = (weights_sum_to_1),
+                       bounds = bounds
+                      )
+    #weights in results.x
+    if disp:
+        return n, er, init_guess, bounds, return_is_target, results, results.x
+    else:
+        return results.x
+    
+
+    
